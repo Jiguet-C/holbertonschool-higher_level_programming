cc4f35336edca7cc65251d646058f1e30b37d83c
Jiguet-C <catherine.jiguet@gmail.com>
2024-05-14 15:43:17 +0200

    add 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
-                for number in row:
-                    print("{:d}".format(number), end=" ")
-                print()
+            for number in row:
+                print("{:d}".format(number), end=" ")
+            print()
