dba40d87f9f21469691fb35c44722fa8cd1b9b2a
Jiguet-C <catherine.jiguet@gmail.com>
2024-05-15 11:14:22 +0200

    add 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+
+    max_nbr = my_list[0]
+    for nbr in my_list[1:]:
+        if nbr > max_nbr:
+            max_nbr = nbr
+        return (max_nbr)
