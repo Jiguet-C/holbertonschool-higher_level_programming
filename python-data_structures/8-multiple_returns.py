65d4b248585fc3bc87df93450b77cc5ed24ee934
Jiguet-C <catherine.jiguet@gmail.com>
2024-05-15 10:43:27 +0200

    add 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length_string = len(sentence)
+    if length_string != 0:
+        first_char = sentence[0]
+    else :
+        first_char = None
+    return (length_string, first_char)
